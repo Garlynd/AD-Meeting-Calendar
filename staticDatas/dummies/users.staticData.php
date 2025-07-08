9b662bf7a7c82e71d4bdaabc1684b774cff75036
Garlynd <weinard21@gmail.com>
2025-07-08 14:58:19 +0800

    feat(staticData): add dummy user data for testing purposes

---
+++ staticDatas/dummies/users.staticData.php
@@ -0,0 +1,11 @@
+<?php
+// filepath: staticData/dummies/users.staticData.php
+return [
+    [
+        'username' => 'john.smith',
+        'first_name' => 'John',
+        'last_name' => 'Smith',
+        'password' => 'p@ssW0rd1234',
+        'role' => 'designer'
+    ],
+];
