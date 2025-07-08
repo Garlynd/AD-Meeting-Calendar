8e8cd13c220149f06f35bf62659a7521273cf8fa
Garlynd <weinard21@gmail.com>
2025-07-08 18:41:23 +0800

    refactor(project_users.model.sql): streamline foreign key references for clarity

--- database/project_users.model.sql
+++ database/project_users.model.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS project_users (
-    project_id INTEGER NOT NULL REFERENCES projects (id),
-    user_id INTEGER NOT NULL REFERENCES users (id),
+    project_id INTEGER NOT NULL REFERENCES projects(id) ON DELETE CASCADE,
+    user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     PRIMARY KEY (project_id, user_id)
 );
